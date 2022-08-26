116f488e4d1c85261713567cf82cb1c072c7676c
Jwelch44 <111645698+Jwelch44@users.noreply.github.com>
2022-08-26 11:44:28 -0400

    Update holidaycheck.py

--- TA-Holiday-Cecker/TA-Holiday-Checker/bin/holidaycheck.py
+++ TA-Holiday-Cecker/TA-Holiday-Checker/bin/holidaycheck.py
@@ -22,8 +22,10 @@ class StreamingHolidayCheck(StreamingCommand):
 
     def stream(self, records):
         for record in records:
-            #record['date_isholiday'] = datetime.datetime.utcfromtimestamp(int(record['time'])).date() in holidays.US(years=2022).keys()
-            #record['date_isholiday'] = '1'
+            record['date_isholiday'] = 'holiday' if datetime.datetime.utcfromtimestamp(int(record['_time'])).date() in [ datetime.date(2022, 1, 1), /
+            datetime.date(2022, 1, 17), datetime.date(2022, 2, 21), datetime.date(2022, 5, 30), datetime.date(2022, 6, 19), datetime.date(2022, 6, 20), /
+            datetime.date(2022, 7, 4), datetime.date(2022, 9, 5), datetime.date(2022, 10, 10), datetime.date(2022, 11, 11), datetime.date(2022, 11, 24), /
+            datetime.date(2022, 12, 25), datetime.date(2022, 12, 26), ] else 'none'
             yield record
 
 dispatch(StreamingHolidayCheck, sys.argv, sys.stdin, sys.stdout, __name__)
