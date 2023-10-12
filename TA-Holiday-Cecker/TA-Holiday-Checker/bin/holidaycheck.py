98cb2cabe44c3e8c487cc12dd3253b98dcd01841
Josh Welch <111645698+Jwelch44@users.noreply.github.com>
2023-10-12 12:52:12 -0400

    Update holidaycheck.py
    add 2024 date's

--- TA-Holiday-Cecker/TA-Holiday-Checker/bin/holidaycheck.py
+++ TA-Holiday-Cecker/TA-Holiday-Checker/bin/holidaycheck.py
@@ -22,11 +22,17 @@ class StreamingHolidayCheck(StreamingCommand):
 
         def stream(self, records):
         for record in records:
-            record['date_isholiday'] = 'holiday' if datetime.datetime.utcfromtimestamp(int(record['_time'])).date() in [ datetime.date(2022, 1, 1), datetime.date(2022, 1, 17), \
-            datetime.date(2022, 2, 21), datetime.date(2022, 5, 30), datetime.date(2022, 6, 19), datetime.date(2022, 6, 20), datetime.date(2022, 7, 4), datetime.date(2022, 9, 5), \
-            datetime.date(2022, 10, 10), datetime.date(2022, 11, 11), datetime.date(2022, 11, 24), datetime.date(2022, 12, 25), datetime.date(2022, 12, 26), datetime.date(2023, 1, 1), \
-            datetime.date(2023, 1, 2), datetime.date(2023, 1, 16), datetime.date(2023, 2, 20), datetime.date(2023, 5, 29), datetime.date(2023, 6, 19), datetime.date(2023, 7, 4), \
-            datetime.date(2023, 9, 4), datetime.date(2023, 10, 9), datetime.date(2023, 11, 11), datetime.date(2023, 11, 10), datetime.date(2023, 11, 23), datetime.date(2023, 12, 25)] \
+            record['date_isholiday'] = 'holiday' if datetime.datetime.utcfromtimestamp(int(record['_time'])).date() in [ datetime.date(2021, 12, 31), \
+            datetime.date(2022, 1, 17), datetime.date(2022, 2, 21), datetime.date(2022, 5, 30), datetime.date(2022, 6, 20), datetime.date(2022, 7, 4), \
+            datetime.date(2022, 9, 5), datetime.date(2022, 10, 10), datetime.date(2022, 11, 11), datetime.date(2022, 11, 24), datetime.date(2022, 12, 26), \
+                                                                                                                        
+            datetime.date(2023, 1, 2), datetime.date(2023, 1, 16), datetime.date(2023, 2, 20), datetime.date(2023, 5, 29), datetime.date(2023, 6, 19), \
+            datetime.date(2023, 7, 4), datetime.date(2023, 9, 4), datetime.date(2023, 10, 9), datetime.date(2023, 11, 11), datetime.date(2023, 11, 23), \
+            datetime.date(2023, 12, 25) \ 
+
+            datetime.date(2024, 1, 1), datetime.date(2024, 1, 15), datetime.date(2024, 2, 19), datetime.date(2024, 5, 27), datetime.date(2024, 6, 19), \
+            datetime.date(2024, 7, 4), datetime.date(2024, 9, 2), datetime.date(2024, 10, 14), datetime.date(2024, 11, 11), datetime.date(2024, 11, 28), \
+            datetime.date(2024, 12, 25)] \
             else 'none'
             yield record
 
